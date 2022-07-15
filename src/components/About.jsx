c7f0629c67ba0049d524f46d52a50a8d7ea1cf8c
Yarik2019 <yroslavterno@gmail.com>
2022-07-16 00:09:58 +0300

    first my portfolio

---
+++ src/components/About.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const About = () => {
+    return(
+        <div name='about' className="w-full h-screen bg-gradient-to-b  from-gray-800 to-black text-white sm:py-10">
+            <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+                <div className="pb-8">
+                    <p className="text-4xl  font-bold inline border-b-4  border-gray-500">About</p>
+                </div>
+                <p className="text-xl mt-20 ms:mt-10">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae deleniti excepturi sit ducimus at architecto laborum labore incidunt aspernatur, possimus facere, nisi doloremque nam, consequuntur asperiores cumque quasi quaerat. Incidunt!
+                    Quidem quae nostrum, consequatur error voluptas magni porro, cupiditate corporis ullam eos perferendis assumenda iure doloribus repellendus minus, sequi amet. Praesentium necessitatibus nesciunt cum aliquam ipsam id aperiam. Porro, autem.
+                </p>
+                <br />
+                <p className="text-xl">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae deleniti excepturi sit ducimus at architecto laborum labore incidunt aspernatur, possimus facere, nisi doloremque nam, consequuntur asperiores cumque quasi quaerat. Incidunt!
+                    Quidem quae nostrum, consequatur error voluptas magni porro, cupiditate corporis ullam eos perferendis assumenda iure doloribus repellendus minus, sequi amet. Praesentium necessitatibus nesciunt cum aliquam ipsam id aperiam. Porro, autem.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default About;
