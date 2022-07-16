6f099942089ee737dcbda8f9c4b74793e3858e68
Yarik2019 <yroslavterno@gmail.com>
2022-07-16 14:33:01 +0300

    bage media

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,7 @@ module.exports = {
     fontFamily: {
       signature: ['Great Vibes']
     },
-    screens: {
-      'lg': '1024px',
-      'md': '768px',
-      'sm': '640px',
-      'xs': '320px'
-    }
+  
 
   },
   plugins: [],
