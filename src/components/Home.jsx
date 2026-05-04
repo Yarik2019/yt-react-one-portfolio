2f7fec54a3630808415ac449497ff06cd3021a41
Ярослав <58841279+Yarik2019@users.noreply.github.com>
2026-05-04 20:35:02 +0300

    fixed header

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -31,7 +31,7 @@ const Home = () => {
           {homeItems.map((item) => (
             <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-between gap-3 h-full px-4 md:flex-row">
               <div className="flex flex-col justify-center h-full">
-                <h2 className="text-4xl sm:text-7xl font-bold text-white">
+                <h2 className="text-4xl sm:text-7xl sm:pt-8 font-bold text-white">
                   {item.title}
                 </h2>
                 <p className="text-gray-500 py-4 max-w-md">
